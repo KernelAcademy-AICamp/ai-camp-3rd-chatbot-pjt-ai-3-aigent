034fc18fd13d7c3844e3259353c40f3ea64f9127
taesikyoon <taesikyoon97@naver.com>
2025-12-09 10:29:25 +0900

    chore

--- _temp/supabase/migrations/20251205062737_e658ff3b-cb7d-42be-8354-b1f776fecb05.sql
+++
@@ -1,6 +0,0 @@
--- Add UPDATE policy for keyword_analyses table to allow toggling favorites
-CREATE POLICY "Users can update their own analyses"
-ON public.keyword_analyses
-FOR UPDATE
-USING (auth.uid() = user_id)
-WITH CHECK (auth.uid() = user_id);
